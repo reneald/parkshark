87b62f9e2bfcab9cae19826358e4c39e64a92de4
reneald <delea.vanreeth@gmail.com>
2018-04-10 15:21:37 +0200

    setup divisionrepositorytest class

--- domain/src/main/java/be/claimed/domain/divisions/DivisionRepository.java
+++ domain/src/main/java/be/claimed/domain/divisions/DivisionRepository.java
@@ -4,17 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
 public class DivisionRepository {
 
+    @PersistenceContext
     private EntityManager entityManager;
-    @Autowired
-    public DivisionRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
 
     public Division create(Division division) {
         entityManager.persist(division);
