193618c049b397cd4df6308f4ce61b6414e109ca
reneald <delea.vanreeth@gmail.com>
2018-04-10 10:56:55 +0200

    script create table divisions

---
+++ scripts/02_create_table_divisions.sql
@@ -0,0 +1,14 @@
+CREATE TABLE divisions
+(
+    id NUMBER(6,0) NOT NULL,
+    name VARCHAR2(50CHAR) NOT NULL,
+    name_orig VARCHAR2(50CHAR) NOT NULL,
+    director VARCHAR2(100CHAR) NOT NULL,
+    fk_parent_div_id NUMBER(6,0) NOT NULL,
+        CONSTRAINT divisions_pk PRIMARY KEY (id),
+        CONSTRAINT divisions_parent_fk FOREIGN KEY (fk_parent_div_id) REFERENCES parkshark.divisions(id)
+);
+
+CREATE SEQUENCE divisions_sequence
+    START WITH 1
+    INCREMENT BY 1;
