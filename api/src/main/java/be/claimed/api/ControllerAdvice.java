940f9591b0c75aa0e60c4a200d86db7da437cb9f
reneald <delea.vanreeth@gmail.com>
2018-04-12 08:44:47 +0200

    LEA logger setup

---
+++ api/src/main/java/be/claimed/api/ControllerAdvice.java
@@ -0,0 +1,8 @@
+package be.claimed.api;
+
+import java.util.logging.Logger;
+
+@org.springframework.web.bind.annotation.ControllerAdvice(basePackages = "be.claimed")
+public class ControllerAdvice {
+    private final static Logger LOGGER = Logger.getLogger("logger");
+}
