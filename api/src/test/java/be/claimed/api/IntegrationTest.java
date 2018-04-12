72d482991f82a2cdd6d9f9c2c1c3e9a4f17ba284
JulienSerbruyns <julien.serbruyns@gmail.com>
2018-04-12 10:54:15 +0200

    Jul & Que some refactoring

--- api/src/test/java/be/claimed/api/IntegrationTest.java
+++
@@ -1,6 +0,0 @@
-package be.claimed.api;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = {TestApplication.class})
-public abstract class IntegrationTest {}
