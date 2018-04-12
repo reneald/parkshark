4f642075f9cbab15b83aea32fd452e7e820ce8ea
reneald <delea.vanreeth@gmail.com>
2018-04-12 08:36:56 +0200

    LEA created MemberController

---
+++ api/src/main/java/be/claimed/api/members/MemberController.java
@@ -0,0 +1,9 @@
+package be.claimed.api.members;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/members")
+public class MemberController {
+}
